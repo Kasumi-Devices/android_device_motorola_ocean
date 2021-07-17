d819c1783d93fd3a34354904e335a8cc087d0aa1
DeinMathelehrer <bruno@suedhoehe25.de>
2021-07-17 14:33:02 +0200

    ocean: adapt for Project Materium & nuke moto apps

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ocean.mk
+    $(LOCAL_DIR)/materium_ocean.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_ocean-eng \
-    lineage_ocean-user \
-    lineage_ocean-userdebug
+    materium_ocean-eng \
+    materium_ocean-user \
+    materium_ocean-userdebug
